236b640d8bbc61650a861cf34f3da805f6a549e8
Onur Karagür <116795501+Onurk77@users.noreply.github.com>
2026-01-12 00:05:04 +0300

    Created configs

---
+++ split_dataset.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+import random 
+import shutil
+
+DATA_DIR = Path(__file__).resolve().parent / "data"
+RAW_DIR = DATA_DIR / "raw"
+OUT_DIR = DATA_DIR / "processed"
+
+SELECTED_CLASSES = [
+    "Actinomyces.israeli",
+    "Bacteroides.fragilis",
+    "Clostridium.perfringens",
+    "Escherichia.coli",
+    "Listeria.monocytogenes",
+]
+
+SEED = 42
+SPLIT = (0.70, 0.20, 0.10) # Train, Val, Test
+IMG_EXTS = {".jpg", ".jpeg", ".png", ".bmp", ".tif", ".tiff"}
