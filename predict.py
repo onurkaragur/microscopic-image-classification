851541083fa01a07fbfaccc2b437e3c2e432eb45
Onur Karagür <116795501+Onurk77@users.noreply.github.com>
2026-01-20 10:21:30 +0300

    load_model function written

--- predict.py
+++ predict.py
@@ -7,3 +7,14 @@ import torch
 import torch.nn as nn
 from torchvision import models, transforms
 
+def load_model(ckpt_path: Path, device: torch.device):
+    ckpt = torch.load(ckpt_path, map_location=device)
+    classes = ckpt["classes"]
+    img_size = ckpt.get("img_size",224)
+
+    model = models.resnet18(weights=None)
+    model.fc = nn.Linear(model.fc.in_features, len(classes))
+    model.load_state_dict(ckpt["model_state"])
+    model.to(device)
+    model.eval()
+    return model, classes, img_size
